49948bb69c138dc99706d4492796a1ee491ac3d1
Eric Golde <eric@golde.org>
2019-12-23 01:17:30 -0800

    theres a better way of doing this

--- PinballMachineNodeMCU.ino
+++ PinballMachineNodeMCU.ino
@@ -1,8 +1,9 @@
 #include "NTPClient.h"
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <ESP8266WiFi.h>
 
-const char *ssid     = "EricDebug";
+const char *ssid = "EricDebug";
 const char *password = "debug1234";
 
 const long utcOffsetInSeconds = -28800;
@@ -11,125 +12,88 @@ const long utcOffsetInSeconds = -28800;
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
-void setup(){
+WiFiServer server(80);
+
+void setup()
+{
   Serial.begin(115200);
+
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
   WiFi.begin(ssid, password);
 
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
-    Serial.print ( "." );
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
   }
 
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+
+  Serial.println("Time Client started");
   timeClient.begin();
 }
 
-void loop() {
+void loop()
+{
   timeClient.update();
 
-  Serial.println(timeClient.getFormattedDay());
-  Serial.println(timeClient.getFormattedTime12());
-  Serial.println(timeClient.getFormattedDate());
-  Serial.println();
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client)
+  {
+    return;
+  }
 
-  delay(1000);
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while (!client.available())
+  {
+    delay(1);
+  }
+
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("Connection: close");
+  client.println(); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+
+  client.println("<html>");
+  client.println("<head>");
+  client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+  client.println("<link rel=\"icon\" href=\"data:,\">");
+  client.println("</head>");
+  client.println("<body>");
+
+  client.print(timeClient.getFormattedDay() + "<br>");
+  client.print(timeClient.getFormattedTime12() + "<br>");
+  client.print(timeClient.getFormattedDate());
+
+  client.println("</body>");
+  client.println("</html>");
+
+  client.stop();
+  Serial.println("Client disonnected");
+  Serial.println("");
 }
-//#include <ESP8266WiFi.h>
-// 
-//const char* ssid = "EricDebug";
-//const char* password = "debug1234";
-// 
-//WiFiServer server(80);
-// 
-//void setup() {
-//  Serial.begin(57600);
-//  delay(10);
-// 
-//  pinMode(LED_BUILTIN, OUTPUT);
-//  digitalWrite(LED_BUILTIN, LOW);
-// 
-//  // Connect to WiFi network
-//  Serial.println();
-//  Serial.println();
-//  Serial.print("Connecting to ");
-//  Serial.println(ssid);
-// 
-//  WiFi.begin(ssid, password);
-// 
-//  while (WiFi.status() != WL_CONNECTED) {
-//    delay(500);
-//    Serial.print(".");
-//  }
-//  Serial.println("");
-//  Serial.println("WiFi connected");
-// 
-//  // Start the server
-//  server.begin();
-//  Serial.println("Server started");
-// 
-//  // Print the IP address
-//  Serial.print("Use this URL to connect: ");
-//  Serial.print("http://");
-//  Serial.print(WiFi.localIP());
-//  Serial.println("/");
-// 
-//}
-// 
-//void loop() {
-//  // Check if a client has connected
-//  WiFiClient client = server.available();
-//  if (!client) {
-//    return;
-//  }
-// 
-//  // Wait until the client sends some data
-//  Serial.println("new client");
-//  while(!client.available()){
-//    delay(1);
-//  }
-// 
-//  // Read the first line of the request
-//  String request = client.readStringUntil('\r');
-//  Serial.println(request);
-//  client.flush();
-// 
-//  // Match the request
-// 
-//  int value = LOW;
-//  if (request.indexOf("/LED=ON") != -1)  {
-//    digitalWrite(LED_BUILTIN, LOW);
-//    value = LOW;
-//  }
-//  if (request.indexOf("/LED=OFF") != -1)  {
-//    digitalWrite(LED_BUILTIN, HIGH);
-//    value = HIGH;
-//  }
-// 
-//// Set LED_BUILTIN according to the request
-////digitalWrite(LED_BUILTIN, value);
-// 
-//  // Return the response
-//  client.println("HTTP/1.1 200 OK");
-//  client.println("Content-Type: text/html");
-//  client.println(""); //  do not forget this one
-//  client.println("<!DOCTYPE HTML>");
-//  client.println("<html>");
-// 
-//  client.print("Led pin is now: ");
-// 
-//  if(value == HIGH) {
-//    client.print("On");
-//  } else {
-//    client.print("Off");
-//  }
-//  client.println("<br><br>");
-//  client.println("<a href=\"/LED=ON\"\"><button>Turn On </button></a>");
-//  client.println("<a href=\"/LED=OFF\"\"><button>Turn Off </button></a><br />");  
-//  client.println("</html>");
-// 
-//  delay(1);
-//  Serial.println("Client disonnected");
-//  Serial.println("");
-// 
-//}
- 
